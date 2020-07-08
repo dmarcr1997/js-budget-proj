1ba3160b6f1f81e8886247695c03dd66e6b224ee
dmarc <dmarcr1997@gmail.com>
2020-07-08 17:17:37 -0500

    added state to display

--- src/form.js
+++ src/form.js
@@ -3,20 +3,24 @@ let getForm = (subFunction, parent) => {
     let budgetLabel = createElem('label',[''], 'Budget') 
     let budget = createElem('input', ['form-control'])
     let budgetButton = createElem('button', ["btn", "btn-success"], 'Calculate Budget')
-    budgetButton.addEventListener('click', subFunction('budget', budget.value))
+    budgetButton.addEventListener('click', event => {
+        subFunction('budget', budget.value)
+        budget.value = ''
+    })
     appendTo(budDiv, [budgetLabel, budget, budgetButton])
     let expenseDiv = createElem('div', ['form-group'])
     let expenseLabel = createElem('label',[''], 'Expense Name') 
     let expense = createElem('input', ['form-control'])
-    let expenseButton = createElem('button', ["btn", "btn-danger"], 'Add Expense')
-    expenseButton.addEventListener('click', subFunction('expense name', expense.value))
-    appendTo(expenseDiv, [expenseLabel, expense, expenseButton])
-
+    appendTo(expenseDiv, [expenseLabel, expense])
     let expenseAmtDiv = createElem('div', ['form-group'])
     let expenseAmtLabel = createElem('label',[''], 'Expense Name') 
     let expenseAmt = createElem('input', ['form-control'])
     let expenseAmtButton = createElem('button', ["btn", "btn-danger"], 'Add Expense')
-    expenseAmtButton.addEventListener('click', subFunction('expense amount', expenseAmt.value))
+    expenseAmtButton.addEventListener('click', event => {
+        subFunction('expense_amount',[expense.value, expenseAmt.value])
+        expense.value = ''
+        expenseAmt.value = ''
+    })
     appendTo(expenseAmtDiv, [expenseAmtLabel, expenseAmt, expenseAmtButton])
     
     appendTo(parent, [budDiv, expenseDiv, expenseAmtDiv])
--- src/render.js
+++ src/render.js
@@ -1,8 +1,19 @@
 let render = () => {
     let parent = document.getElementById('main')
+    state = {
+        'budget': 0,
+        'expenses': []
+    }
     renderContent = () => {
-        let changeState = (data) => {
-            state = {data}
+        let changeState = (type, data) => {
+            if (data != ''){
+                if (type == 'budget'){
+                    state['budget'] = data
+                } else{
+                state['expenses'].push(data)
+                }
+            }
+            debugger
         }
         getForm(changeState, parent)
         getDisplay(state)
