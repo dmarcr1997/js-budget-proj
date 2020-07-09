49d881080aec909fee1afabfb052458ff4a12dcd
dmarc <dmarcr1997@gmail.com>
2020-07-08 22:14:31 -0500

    created balance display

--- src/budgetDisplay.js
+++ src/budgetDisplay.js
@@ -1,10 +1,12 @@
-let getDisplay = (budget, expenseContainer, state) => {
+let getDisplay = (budget, expenseContainer, bal, state) => {
 
   budget.innerText = state.budget
   let expns =  [...new Set(state.expenses)]
+  expenseContainer.innerHTML= ''
   expns.map(exp => {
       let li = createElem('li', [''])
       li.innerText = `${exp[0]}: $ ${exp[1]}`
       expenseContainer.appendChild(li)
   })
+  bal.innerText = state.balance
 }
--- src/render.js
+++ src/render.js
@@ -3,31 +3,49 @@ let render = () => {
     parent.classList.add('container')
     state = {
         'budget': 0,
-        'expenses': []
+        'expenses': [],
+        'balance': 0
     }
     renderContent = () => {
         let changeState = (type, data) => {
-            if (data != ''){
+            if (data != '' ){
                 if (type == 'budget'){
                     state['budget'] = data
+                    state.balance= data 
+                    getDisplay(budgetDis, expensesDis, balance, state)
                 } else{
-                    state['expenses'].push(data)
+                    if(data[0] != "" && data[1] != "" ){
+                        state.balance -= parseFloat(data[1])
+                        state['expenses'].push(data)
+                        getDisplay(budgetDis, expensesDis, balance, state)
+                    }
                 }
+                
             }
             console.log(state)
-            getDisplay(budgetDis, expensesDis, state)
         }
         getForm(changeState, parent)
+        let budgetHead = createElem('h2', [''])
+        budgetHead.innerText ='Budget'
         let budgetDis = createElem('h3', [''])
         budgetDis.innerText = state.budget
         let moneyBag = document.createElement('img')
         moneyBag.src = "https://img.icons8.com/dotty/80/000000/money-bag-shekel-.png"
+        
+        let expenseHead = createElem('h2', [''])
+        expenseHead.innerText ='Expenses'
         let expensesDis = createElem('ul', [''])
         let expnsRep = document.createElement('img')
         expnsRep.src = "https://img.icons8.com/pastel-glyph/64/000000/transaction-list--v5.png"
         expensesDis.innerText = state.expenses
-
-        appendTo(parent, [budgetDis, moneyBag, expensesDis, expnsRep])
+        
+        let balanceHead = createElem('h2', [''])
+        balanceHead.innerText ='Balance'
+        let balance = createElem('h3', [''])
+        balance.innerText = state.balance
+        let balRep = document.createElement('img')
+        balRep.src = "https://img.icons8.com/pastel-glyph/64/000000/invoice-1.png"
+        appendTo(parent, [budgetHead, moneyBag, budgetDis, expenseHead, expnsRep, expensesDis, balanceHead, balRep, balance])
     }
     return renderContent
 }
